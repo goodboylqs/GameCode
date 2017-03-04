f08d91abe532de14b5172bfcef94d3796e8af022
simpleboy95 <553912917@qq.com>
2017-03-04 17:36:16 +0800

    由于之前在Demo文件中已经完成了所有功能的修改，而原本的cpp文件未完成，因此修改了关于Mouse Message的两个文件的文件名，将原本的Demo文件末尾的Demo去掉，原本的cpp文件后面加上了Demo标记，Demo功能未完成但将不再修改

--- GameChapter7/GameChapter7_MouseMessage/Windows_Mouse_Message_Demo.cpp
+++ GameChapter7/GameChapter7_MouseMessage/Windows_Mouse_Message_Demo.cpp
@@ -1,26 +1,19 @@
 //----------------------【程序说明】----------------------------------------------------
-//文件名：Windows_Mouse_Message_Demp.cpp
-//内容：Windows鼠标消息响应程序的Demo，用来测试功能是否被正确实现
-//起始时间：2017.2.17     17:26   
-//修改记录：1)2017.2.21   12:32      加入对子弹对象  
-//			2)2017.3.1    23:58      修改子弹对象的贴图规则
-//状态：完成
-//          
-//说明：所需的图片放置与当前文件放置在同一目录，直接使用图片名即可。
+//文件名：Windows_Mouse_Message_Demo.cpp
+//内容：Windows鼠标消息响应程序示例
+//起始时间：2017.1.16   21:41   By lqs
+//修改记录：1)2017.1.18   00:11   GameInit函数完成了一半，WndProc函数完成  
+//			2)2017.1.19   08:51   GameInit函数完成，Game_Paint函数未完成
+//			3)2017.3.4    17:30   在文件Windows_Mouse_Message.cpp中已经完成了所有功能，该文件不再修改
+//状态：
+//说明：
 //--------------------------------------------------------------------------------------
 //-------------------------【宏定义】---------------------------------------------------
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define SWORDMAN_WIDTH 317
-#define SWORDMAN_HEIGHT 283
 //--------------------------------------------------------------------------------------
 //---------------------【头文件】-------------------------------------------------------
 #include<Windows.h>
-#include<iostream>
-using namespace std;
-//--------------------------------------------------------------------------------------
-//---------------------【库文件】-------------------------------------------------------
-#pragma comment(lib,"msimg32.lib")
 //--------------------------------------------------------------------------------------
 //------------------------------【结构体定义】------------------------------------------
 struct SwoardBullets {
@@ -32,31 +25,24 @@ struct SwoardBullets {
 DWORD g_tNow, g_tPre;            //记录上一次循环的时间和当前时间
 HDC g_hdc = NULL, g_mdc = NULL, g_bufdc = NULL;
 HBITMAP g_hBackground = NULL, g_hSwordMan = NULL, g_hSwordBlade = NULL;
-int g_sX = 300, g_sY = 100;                 //人物坐标
-SwoardBullets Bullets[30] = {0,0,false};           //子弹数组
-int g_iBGOffset = 0, g_iBulletNum = 0;   //g_iBGOffset为滚动背景所要裁剪的区域的宽度
-										 //g_iBulletNum为还存在的子弹的数目
-//--------------------------------------------------------------------------------------
-//------------------------------【函数声明】--------------------------------------------
-BOOL Game_Init(HWND hwnd);
-VOID Game_Paint(HWND hwnd);
-VOID Game_CleanUp(HWND hwnd);
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+int g_iX = 0, g_iY = 0, g_iYnow = 0, g_iXnow = 0,g_iBulletNum = 0;  //g_iX,g_iY代表鼠标光标的坐标，g_iXnow人物坐标
+int g_iBGOffset = 0, g_iBulletNum = 0;    //g_iBGOffset为滚动背景所要裁剪的区域的宽度
+SwoardBullets Bullet[30];          //子弹数组
 //--------------------------------------------------------------------------------------
 
 //------------------------【WinMain函数】-----------------------------------------------
 //功能：Windows程序的入口函数
-//起始时间：2017.2.17  17:32
+//起始时间：2017.1.16  22:27
 //修改记录：
-//状态: 1)2017.2.17  17:32    起始
+//状态:
 //说明：
-//最后修改时间：
+//最后修改时间：2017.1.16  22:27
 //---------------------------------------------------------------------------------------
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd)
 {
 	//第一步：设计窗口类
 	WNDCLASSEX wndClass = { 0 };          //定义窗口类
-	wndClass.cbSize = sizeof(WNDCLASSEX);   //窗口的字节数
+	wndClass.cbSize = sizeof(WNDCLASS);   //窗口的字节数
 	wndClass.style = CS_HREDRAW | CS_VREDRAW;   //窗口央视
 	wndClass.lpfnWndProc = WndProc;      //指定窗口过程函数
 	wndClass.cbClsExtra = 0;								//窗口类的附加内存，取0就可以了
@@ -67,29 +53,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);  //为hbrBackground成员指定一个白色画刷句柄	
 	wndClass.lpszMenuName = NULL;						//用一个以空终止的字符串，指定菜单资源的名字。
 	wndClass.lpszClassName = L"GameWindow";        //指定窗口类的名字
-												   //第二步：注册窗口类
+	//第二步：注册窗口类
 	if (!RegisterClassEx(&wndClass))
+	{
 		return -1;
-	
+	}
 	//第三步：正式创建窗口类
 	HWND hwnd = CreateWindowW(L"GameWindow",
-		L"Windows鼠标消息响应程序示例",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
-		NULL, NULL, hInstance, NULL);
+								L"Windows鼠标消息响应程序示例",
+								WS_OVERLAPPEDWINDOW,
+								CW_USEDEFAULT,
+								CW_USEDEFAULT,
+								WINDOW_WIDTH,
+								WINDOW_HEIGHT,
+								NULL,NULL,hInstance,NULL);
 	//第四步：窗口的移动显示与更新
 	MoveWindow(hwnd, 200, 20, WINDOW_WIDTH, WINDOW_HEIGHT, true);		//调整窗口显示时的位置，使窗口左上角位于（200,20）处
 	ShowWindow(hwnd, nShowCmd);    //调用ShowWindow函数来显示窗口
 	UpdateWindow(hwnd);						//对窗口进行更新，就像我们买了新房子要装修一样
 
-	if (!Game_Init(hwnd))              //窗口建立后，对游戏内容进行初始化
-	{
-		MessageBox(hwnd, L"初始化失败！", L"消息窗口", 0);
-		return FALSE;
-	}
 	//第五步：消息循环过程
 	MSG msg = { 0 };        //定义消息
 	while (msg.message != WM_QUIT)     //如果不是WM_QUIT消息就继续循环
@@ -104,7 +86,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			g_tNow = GetTickCount();
 			if (g_tNow - g_tPre >= 100)
 			{
-				Game_Paint(hwnd);
+				Game_Init(hwnd);
 			}
 		}
 	}
@@ -112,16 +94,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	UnregisterClass(L"GameWindow", wndClass.hInstance);  //程序准备结束，注销窗口类
 	return 0;
 }
-//------------------------【WndProc函数】-----------------------------------------------
-//功能：窗口过程函数
-//起始时间：2017.2.17  17:32
-//修改记录：1)2017.2.21    13:07    加入对鼠标移动消息的响应，注：要将屏幕坐标转为窗口坐标
-//		    2)2017.2.1     23:49    修改鼠标点击时间的响应
-//状态: 完成
-//说明：
-//最后修改时间：
-//---------------------------------------------------------------------------------------
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
 {
 	switch (message)
 	{
@@ -136,55 +109,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		break;
 	case WM_LBUTTONDOWN:
-	{
 		for (int i = 0; i < 30; ++i)
 		{
-			if (Bullets[i].exist == false)
+			if (!Bullet[i].exist)
 			{
-				Bullets[i].x = g_sX;
-				Bullets[i].y = g_sY + 30;
-				Bullets[i].exist = true;
+				Bullet[i].x = g_iXnow;    
+				Bullet[i].y = g_iYnow + 30;
+				Bullet[i].exist = true;
 				g_iBulletNum++;
-				break;
 			}
 		}
-	}
 		break;
-	case WM_MOUSEMOVE:     //控制人物向鼠标移动的方向前进
-	{
-		POINT Curpos_new;              //记录当前鼠标坐标的结构体
-		GetCursorPos(&Curpos_new);
-		ScreenToClient(hwnd,&Curpos_new);     //要把屏幕坐标转换为窗口坐标
-		if (Curpos_new.x == g_sX && Curpos_new.y == g_sY)break;
-		if (Curpos_new.x < g_sX)
-		{
-			if (Curpos_new.x < g_sX - 10)
-				g_sX -= 10;
-			else
-				g_sX = Curpos_new.x;
-		}
-		else 
-		{
-			if (Curpos_new.x > g_sX + 10)
-				g_sX += 10;
-			else
-				g_sX = Curpos_new.x;
-		}
-		if (Curpos_new.y < g_sY)
-		{
-			if (Curpos_new.y < g_sY - 10)
-				g_sY -= 10;
-			else
-				g_sY = Curpos_new.y;
-		}
-		else
-		{
-			if (Curpos_new.y > g_sY + 10)
-				g_sY += 10;
-			else
-				g_sY = Curpos_new.y;
-		}
-	}
+	case WM_MOUSEMOVE:
+		//对x坐标的处理
+		g_iX = LOWORD(lParam);      //取得鼠标x坐标
+		if (g_iX > WINDOW_WIDTH - 317)    //设置临界坐标
+			g_iX = WINDOW_WIDTH - 317;
+		else if (g_iX < 0)
+			g_iX = 0;
+		//对y坐标的处理
+		g_iY = HIWORD(lParam);
+		if (g_iY > WINDOW_HEIGHT - 283)
+			g_iY = WINDOW_HEIGHT - 283;
+		else if (g_iY < -200)
+			g_iY = -200;
 		break;
 	case WM_DESTROY:
 		Game_CleanUp(hwnd);
@@ -196,14 +144,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 	return 0;
 }
-//------------------------【Game_Init函数】-----------------------------------------------
-//功能：载入资源
-//起始时间：2017.2.17  17:32
-//修改记录：1)2017.2.17  19:30   完成所有图片资源的载入
-//状态: 完成
-//说明：
-//最后修改时间：
-//---------------------------------------------------------------------------------------
 BOOL Game_Init(HWND hwnd)
 {
 	HBITMAP bmp;
@@ -211,117 +151,123 @@ BOOL Game_Init(HWND hwnd)
 	g_hdc = GetDC(hwnd);
 	g_mdc = CreateCompatibleDC(g_hdc);
 	g_bufdc = CreateCompatibleDC(g_hdc);
-	bmp = CreateCompatibleBitmap(g_hdc, WINDOW_WIDTH, WINDOW_HEIGHT);
+	bmp = CreateCompatibleBitmap(g_hdc,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+	//设定人物贴图初始位置，鼠标位置初始值
+	g_iX = 300;
+	g_iY = 100;
+	g_iXnow = 100;
+	g_iYnow = 100;
 
 	SelectObject(g_mdc, bmp);
 	//加载各张跑动图以及背景图
-	g_hSwordMan = (HBITMAP)LoadImage(NULL, L"swordman.bmp", IMAGE_BITMAP, 317, 283, LR_LOADFROMFILE);
-	g_hSwordBlade = (HBITMAP)LoadImage(NULL, L"swordblade.bmp", IMAGE_BITMAP, 100, 26, LR_LOADFROMFILE);
+	g_hSwordMan = (HBITMAP)LoadImage(NULL,L"swordman.bmp",IMAGE_BITMAP,317,283,LR_LOADFROMFILE);
+	g_hSwordBlade = (HBITMAP)LoadImage(NULL,L"swordblade.bmp",IMAGE_BITMAP,100,26,LR_LOADFROMFILE);
 	g_hBackground = (HBITMAP)LoadImage(NULL, L"bg.bmp", IMAGE_BITMAP, WINDOW_WIDTH, WINDOW_HEIGHT, LR_LOADFROMFILE);
-	
-	return TRUE;
-}
-//------------------------【Game_Paint函数】-----------------------------------------------
-//功能：贴图函数
-//起始时间：2017.2.17  17:32
-//修改记录：1)2017.2.17    17:32     开始
-//          2)2017.2.17    19:30     完成背景贴图功能，人物贴图需要解决透明色彩问题
-//			3)2017.3.1	   23:48     修改子弹贴图规则，使得子弹贴图的坐标岁时间变化，且
-//									 子弹超出窗口范围的子弹自动设为隐藏
-//状态: 完成
-//说明：
-//最后修改时间：
-//---------------------------------------------------------------------------------------
-VOID Game_Paint(HWND hwnd)
-{
-	g_tPre = GetTickCount();
-	g_iBGOffset += 5;
-	if (g_iBGOffset > WINDOW_WIDTH)
-		g_iBGOffset = 0;
-	//限制光标的移动范围只能在窗口内
-	//RECT *Available_Rect = new RECT();
-	//GetWindowRect(hwnd, Available_Rect);       //获取窗口的范围
-	//ClipCursor(Available_Rect);      //设置光标移动范围
+
 
 	POINT pt, lt, rb;
 	RECT rect;
+	//设定光标位置
+	pt.x = 300;
+	pt.y = 100;
+	ClientToScreen(hwnd,&pt);      //窗口坐标转换为屏幕坐标
+	SetCursorPos(pt.x,pt.y);       //设置光标位置
 
-	ShowCursor(false);
+	ShowCursor(false);             //隐藏光标位置
 
-	//限制鼠标光标移动区域
-	GetClientRect(hwnd, &rect);
-	//将窗口左上顶点坐标存入lt中
+	//限制鼠标光标移动的区域
+	GetClientRect(hwnd,&rect);     //取得窗口内部矩形
+	//将矩形左上角坐标存入lt中
 	lt.x = rect.left;
 	lt.y = rect.top;
-	//右下顶点坐标存入rb中
-	rb.x = rect.right;
-	rb.y = rect.bottom;
-	//将lt和rb的坐标转换为窗口坐标
-	ClientToScreen(hwnd, &lt);
-	ClientToScreen(hwnd, &rb);
-	//以屏幕坐标重新设定巨型区域
+	//将lt和rb的窗口坐标转化为屏幕坐标
+	ClientToScreen(hwnd,&lt);
+	ClientToScreen(hwnd,&rb);
+	//以屏幕坐标重新设定区域
 	rect.left = lt.x;
 	rect.top = lt.y;
 	rect.right = rb.x;
 	rect.bottom = rb.y;
-	//限制鼠标光标以东区域
+	//限制鼠标移动区域
 	ClipCursor(&rect);
 
+	Game_Paint(hwnd);
+	return true;
+}
+VOID Game_Paint(HWND hwnd)
+{
+	//现在mdc中贴上背景图
+	SelectObject(g_mdc, g_hBackground);
+	BitBlt(g_mdc, 0, 0, g_iBGOffset, WINDOW_HEIGHT, g_bufdc,
+		WINDOW_WIDTH - g_iBGOffset, 0, SRCCOPY);
+	BitBlt(g_mdc, g_iBGOffset, 0, WINDOW_WIDTH - g_iBGOffset,
+		WINDOW_HEIGHT, g_bufdc, 0, 0, SRCCOPY);
 
-	//贴背景
-	SelectObject(g_bufdc,g_hBackground);
-	BitBlt(g_mdc, 0, 0, g_iBGOffset, WINDOW_HEIGHT, g_bufdc, WINDOW_WIDTH - g_iBGOffset, 0, SRCCOPY);
-	BitBlt(g_mdc, g_iBGOffset, 0, WINDOW_WIDTH - g_iBGOffset, WINDOW_HEIGHT, g_bufdc, 0, 0, SRCCOPY);
+	wchar_t str[20] = {};
 
-	SelectObject(g_bufdc, g_hSwordMan);
-	TransparentBlt(g_mdc, g_sX, g_sY, 317, 283, g_bufdc, 0, 0, 317,283,RGB(0,0,0));
+	//计算人物的贴图坐标，每次进行剑侠贴图时，其贴图坐标(g_iXnow,g_iYnow)会
+	//慢慢向鼠标光标所在坐标(x,y)移动，直到两个坐标相同为止
+	if (g_iXnow < g_iX)
+	{
+		g_iXnow += 10;
+		if (g_iXnow > g_iX)
+			g_iXnow = g_iX;
+	}
+	else
+	{
+		g_iXnow -= 10;
+		if (g_iXnow < g_iX)
+			g_iXnow = g_iX;
+	}
+	if (g_iYnow < g_iY)
+	{
+		g_iYnow += 10;
+		if (g_iYnow > g_iY)
+			g_iYnow = g_iY;
+	}
+	else
+	{
+		g_iYnow -= 10;
+		if (g_iYnow < g_iY)
+			g_iYnow = g_iY;
+	}
 
-	//贴子弹
-	SelectObject(g_bufdc,g_hSwordBlade);
-	
+	//贴上人物图
+	SelectObject(g_bufdc,g_hSwordMan);
+	TransparentBlt(g_mdc, g_iXnow, g_iYnow, 317, 283, g_bufdc, 0, 0, 317, 283, RGB(0, 0, 0));
+
+	//子弹的贴图，先判断子弹数目"g_iBulletNum"的值是否为0，若不为0，则对子弹数组中各个
+	//还存在的子弹按照其所在的坐标(b[i].x,b[i].y)循环进行贴图操作
+	SelectObject(g_bufdc, g_hSwordBlade);
 	if (g_iBulletNum != 0)
 	{
 		for (int i = 0; i < 30; ++i)
 		{
-			if (Bullets[i].exist == true)
+			if (Bullet[i].exist)
 			{
-				TransparentBlt(g_mdc, Bullets[i].x, Bullets[i].y, 100, 33, g_bufdc, 0, 0,
-					100, 26, RGB(0, 0, 0));
-				Bullets[i].x -= 10;
-				if (Bullets[i].x < 0)
+				//贴图子弹
+				TransparentBlt(g_mdc, Bullet[i].x - 70, Bullet[i].y + 100,
+					100, 33, g_bufdc, 0, 0, 100, 26, RGB(0, 0, 0));
+
+				//设置下一个子弹的坐标。子弹是从右向左发射的，因此，每次起X轴
+				//上的坐标值减少10个单位，这样贴图就会产生往左移动的效果。
+				//如果子弹下次的坐标已经查出窗口的可见范围(h[i].x < 0)，那么子弹不存在
+				//并将子弹总数g_iBulletNum变量值减1.
+				Bullet[i].x -= 10;
+				if (Bullet[i].x < 0)
 				{
-					Bullets[i].exist = false;
 					g_iBulletNum--;
-				}	
+					Bullet[i].exist = false;
+				}
 			}
+			HFONT hFont;
+			hFont = CreateFont(20,0,0,0,0,0,0,0,GB2312_CHARSET,)
 		}
 	}
 
-	BitBlt(g_hdc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, g_mdc, 0, 0, SRCCOPY);
 }
-//------------------------【Game_CleanUp函数】-----------------------------------------------
-//功能：待程序退出时进行资源清理
-//起始时间：2017.2.17  17:32
-//修改记录：
-//状态: 待加入对子弹对象的清理
-//说明：
-//最后修改时间：2017.2.17   17:40
-//---------------------------------------------------------------------------------------
 VOID Game_CleanUp(HWND hwnd)
 {
-	DeleteObject(g_hBackground);
-	DeleteObject(g_hSwordBlade);
-	DeleteObject(g_hSwordMan);
 
-	ReleaseDC(hwnd,g_bufdc);
-	ReleaseDC(hwnd,g_mdc);
-	DeleteDC(g_hdc);
 }
-//------------------------【Game_Paint_Bullet函数】-----------------------------------------------
-//功能：绘制子弹
-//起始时间：2017.2.20  19:09
-//修改记录：
-//状态: 
-//说明：对鼠标左键消息进行响应，点一下绘制一个新的子弹
-//最后修改时间：
-//---------------------------------------------------------------------------------------
